897a86c4ccba37f99a56ce27820683024d551ac2
Quang Ngoc <21020550@vnu.edu.vn>
2023-10-12 19:04:48 +0700

    update auto run

--- main.py
+++ main.py
@@ -1,3 +1,48 @@
+
+from config import LOCAL_ABSOLUTE_PATH
 import os
 
-os.system("python3 models/Light_GBM_gross_bootstraping/EXP_LGBM/gradual/window_100/Lgbm_gradual_weighted_widnow100.py")
+RUN_CMD = 'python3 ' + LOCAL_ABSOLUTE_PATH
+
+# RUN LGBM MODEL
+
+# EXP_LGBM
+# os.system(RUN_CMD + "models/Light_GBM_gross_bootstraping/EXP_LGBM/gradual/window_100/Lgbm_gradual_weighted_widnow100.py") already run
+os.system(RUN_CMD + "models/Light_GBM_gross_bootstraping/EXP_LGBM/gradual/window_200/Lgbm_gradual_weighted_widnow200.py")
+os.system(RUN_CMD + "models/Light_GBM_gross_bootstraping/EXP_LGBM/gradual/window_All/Lgbm_gradual_weighted_widnow_all.py")
+os.system(RUN_CMD + "models/Light_GBM_gross_bootstraping/EXP_LGBM/incremental/window_100/Lgbm_incremental_weighted_widnow100.py")
+os.system(RUN_CMD + "models/Light_GBM_gross_bootstraping/EXP_LGBM/incremental/window_200/Lgbm_incremental_weighted_widnow200.py")
+os.system(RUN_CMD + "models/Light_GBM_gross_bootstraping/EXP_LGBM/incremental/window_After/Lgbm_incremental_weighted_widnow_After.py")
+os.system(RUN_CMD + "models/Light_GBM_gross_bootstraping/EXP_LGBM/incremental/window_All/Lgbm_incremental_weighted_widnow_All.py")
+os.system(RUN_CMD + "models/Light_GBM_gross_bootstraping/EXP_LGBM/sudden/window_100/Lgbm_sudden_weighted_widnow100.py")
+os.system(RUN_CMD + "models/Light_GBM_gross_bootstraping/EXP_LGBM/sudden/window_200/Lgbm_sudden_weighted_widnow200.py")
+os.system(RUN_CMD + "models/Light_GBM_gross_bootstraping/EXP_LGBM/sudden/window_After/Lgbm_sudden_weighted_widnow_After.py")
+os.system(RUN_CMD + "models/Light_GBM_gross_bootstraping/EXP_LGBM/sudden/window_all/Lgbm_sudden_weighted_widnowAll.py")
+
+# Linear_LGBM
+os.system(RUN_CMD + "models/Light_GBM_gross_bootstraping/Linear_LGBM/gradual/window_100/Linear_Lgbm_gradual_weighted_widnow100.py")
+os.system(RUN_CMD + "models/Light_GBM_gross_bootstraping/Linear_LGBM/gradual/window_200/Linear_Lgbm_gradual_weighted_widnow200.py")
+os.system(RUN_CMD + "models/Light_GBM_gross_bootstraping/Linear_LGBM/gradual/window_All/Linear_Lgbm_gradual_weighted_widnow_all.py")
+os.system(RUN_CMD + "models/Light_GBM_gross_bootstraping/Linear_LGBM/incremental/window_100/Linear_Lgbm_incremental_weighted_widnow100.py")
+os.system(RUN_CMD + "models/Light_GBM_gross_bootstraping/Linear_LGBM/incremental/window_200/Linear_Lgbm_incremental_weighted_widnow200.py")
+os.system(RUN_CMD + "models/Light_GBM_gross_bootstraping/Linear_LGBM/incremental/window_After/Linear_Lgbm_incremental_weighted_widnow_After.py")
+os.system(RUN_CMD + "models/Light_GBM_gross_bootstraping/Linear_LGBM/incremental/window_All/Linear_Lgbm_incremental_weighted_widnow_All.py")
+os.system(RUN_CMD + "models/Light_GBM_gross_bootstraping/Linear_LGBM/sudden/window_100/Linear_Lgbm_sudden_weighted_widnow_100.py")
+os.system(RUN_CMD + "models/Light_GBM_gross_bootstraping/Linear_LGBM/sudden/window_200/Linear_Lgbm_sudden_weighted_widnow200.py")
+os.system(RUN_CMD + "models/Light_GBM_gross_bootstraping/Linear_LGBM/sudden/window_After/Linear_Lgbm_sudden_weighted_widnow_After.py")
+os.system(RUN_CMD + "models/Light_GBM_gross_bootstraping/Linear_LGBM/sudden/window_all/Linear_Lgbm_sudden_weighted_widnowAll.py")
+
+# Plain_LGBM
+os.system(RUN_CMD + "models/Light_GBM_gross_bootstraping/Plain_LGBM/gradual/window_100/Lgbm_gradual_weighted_widnow100.py")
+os.system(RUN_CMD + "models/Light_GBM_gross_bootstraping/Plain_LGBM/gradual/window_200/Lgbm_gradual_weighted_widnow200.py")
+os.system(RUN_CMD + "models/Light_GBM_gross_bootstraping/Plain_LGBM/gradual/window_All/Lgbm_gradual_weighted_widnow_all.py")
+os.system(RUN_CMD + "models/Light_GBM_gross_bootstraping/Plain_LGBM/incremental/window_100/Lgbm_incremental_weighted_widnow100.py")
+os.system(RUN_CMD + "models/Light_GBM_gross_bootstraping/Plain_LGBM/incremental/window_200/Lgbm_incremental_weighted_widnow200.py")
+os.system(RUN_CMD + "models/Light_GBM_gross_bootstraping/Plain_LGBM/incremental/window_After/Lgbm_incremental_weighted_widnow_After.py")
+os.system(RUN_CMD + "models/Light_GBM_gross_bootstraping/Plain_LGBM/incremental/window_All/Lgbm_incremental_weighted_widnow_All.py")
+os.system(RUN_CMD + "models/Light_GBM_gross_bootstraping/Plain_LGBM/sudden/window_100/Lgbm_sudden_weighted_widnow100.py")
+os.system(RUN_CMD + "models/Light_GBM_gross_bootstraping/Plain_LGBM/sudden/window_200/Lgbm_sudden_weighted_widnow200.py")
+os.system(RUN_CMD + "models/Light_GBM_gross_bootstraping/Plain_LGBM/sudden/window_After/Lgbm_sudden_weighted_widnow_After.py")
+os.system(RUN_CMD + "models/Light_GBM_gross_bootstraping/Plain_LGBM/sudden/window_all/Lgbm_sudden_weighted_widnowAll.py")
+
+# RUN GDW_ECW
